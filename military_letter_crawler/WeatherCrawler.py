efb0071bfbbda3aeff73898c0019bcbe2e7d7375
S4nop <rshtiger@naver.com>
2020-04-01 01:10:20 +0900

    Weather crawler class implemented

---
+++ military_letter_crawler/WeatherCrawler.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+class WeatherCrawler:
+
+    def parseWeatherInfo(self, tBodySoup, mode = 0):
+        mTemp = tBodySoup.select(".cell")[0].select(".temp")[0].get_text()
+        aTemp = tBodySoup.select(".cell")[1].select(".temp")[0].get_text()
+
+        if mode == 1:
+            date = "내일"
+            mSplit = tBodySoup.select(".cell")[0].select(".info")[0].get_text().split("강수확률")
+            aSplit = tBodySoup.select(".cell")[1].select(".info")[0].get_text().split('강수확률')
+            morning = mTemp + "℃ - " + mSplit[0] + ' - 강수확률' + mSplit[1]
+            afternoon = aTemp + "℃ - " + aSplit[0] + ' - 강수확률 ' + aSplit[1]
+        else:
+            date = tBodySoup.select("th")[0].get_text()
+            morning = mTemp + "℃ - " + tBodySoup.select(".cell")[0].select(".info")[0].get_text()
+            afternoon = aTemp + "℃ - " + tBodySoup.select(".cell")[1].select(".info")[0].get_text()
+
+        return (date, morning, afternoon)
+
+    def getWeather(self):
+        req = requests.get("https://weather.naver.com/rgn/townWetr.nhn?naverRgnCd=15230253")
+        soup = BeautifulSoup(req.content, 'html.parser')
+        respSoup = soup.select("table")
+
+        result = []
+        result.append(self.parseWeatherInfo(respSoup[1].select("td")[1], 1))
+        for child in respSoup[2].select("tr"):
+            result.append(self.parseWeatherInfo(child, 0))
+
+        return result
+
+
+if __name__ == "__main__":
+    wc = WeatherCrawler()
+    print(wc.getWeather())
