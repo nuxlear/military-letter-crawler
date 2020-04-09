6ac817e003c5cd511f7dbcbb5f32715916a13115
worldedi <shh9904@gmail.com>
2020-04-09 23:35:02 +0900

    crawler intiation code for soldiers

--- military_letter_crawler.py
+++ military_letter_crawler.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from bs4 import Comment
 from enum import Enum
 import re
-
+from datetime import datetime
 # --Class PageFeed : Get facebook page/group's writing--
 '''
 Usage
@@ -424,11 +424,58 @@ class WeatherCrawler:
 
 
 if __name__ == "__main__":
-
+    client=LetterClient()
+    client.login('julia981028@gmail.com', 'qwer1234!')
     fbc = FacebookCrawler()
-    fbc.set_user('ryu')
+    fbc.set_user('hwang')
+    fbc.addPagetoList('SKKUBamboo',5)
+    fbc.addPagetoList('yonseibamboo',5)
+    fbc.addGrouptoList('KerasKorea', 5)
+    fbc.addGrouptoList('System.out.Coding',5)
     fbcResult = fbc.autoRunFromFile()
-    print(fbcResult)
+    #print(fbcResult)
+    client.send_letter("황준원","facebook M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), fbcResult)
+    client.send_letter("강원모","facebook M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), fbcResult)
+    
+    wtc = WeatherCrawler()
+    wtcResult = wtc.getWeather()
+    client.send_letter("황준원","weather M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), wtcResult)
+    client.send_letter("강원모","weather M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), wtcResult)
+    
+    naverResult = []
+    googleResult = []
+    coronaResult = []
+    nc = NewsCrawler().NaverNews()
+    nc.getNewsPage()
+    naverResult.append(nc.getNewsTitles(nc.NewsType.LIFECULTURE))
+    naverResult.append(nc.getNewsTitles(nc.NewsType.WORLD))
+    naverResult.append(nc.getNewsTitles(nc.NewsType.ECONOMY))
+    naverResult.append(nc.getNewsTitles(nc.NewsType.POLITIC))
+    naverResult.append(nc.getNewsTitles(nc.NewsType.SOCIETY))
+    naverResult.append(nc.getNewsTitles(nc.NewsType.ITSCIENCE))
+    client.send_letter("황준원","NAVERN M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), str(naverResult))
+    client.send_letter("강원모","NAVERN M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), str(naverResult))
+    
+    '''gc = NewsCrawler().GoogleNews()
+    googleResult.append(gc.getNewsTitles(gc.CONST_HEADLINE_URL,5))
+    googleResult.append(gc.getNewsTitles(gc.CONST_KOR_URL,5))
+    googleResult.append(gc.getNewsTitles(gc.CONST_WORLD_URL,5))
+    googleResult.append(gc.getNewsTitles(gc.CONST_BUSINESS_URL,5))
+    googleResult.append(gc.getNewsTitles(gc.CONST_SCI_TECH_URL,5))
+    googleResult.append(gc.getNewsTitles(gc.CONST_ENTERTAIN_URL,5))
+    googleResult.append(gc.getNewsTitles(gc.CONST_SPORTS_URL,5))
+    googleResult.append(gc.getNewsTitles(gc.CONST_HEALTH_URL,5))
+    client.send_letter("황준원","googleN M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), str(googleResult))
+    client.send_letter("강원모","googleN M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), str(googleResult))'''
+
+    cc = NewsCrawler().Corona()
+    coronaResult.append(cc.getTodayData())
+    client.send_letter("황준원","coronaN M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), str(coronaResult))
+    client.send_letter("강원모","coronaN M:"+str(datetime.today().month)+' D:'+str(datetime.today().day), str(coronaResult))
+    #fbc = FacebookCrawler()
+    #fbc.set_user('ryu')
+    #fbcResult = fbc.autoRunFromFile()
+    #print(fbcResult)
     #wtc = WeatherCrawler()
     #wtcResult = wtc.getWeather()
 
@@ -441,7 +488,7 @@ if __name__ == "__main__":
     #cc = NewsCrawler.Corona()
     #newsList.append(cc.getTodayData())
 
-    lc = LetterClient()
+    #lc = LetterClient()
 
-    lc.login("rshtiger@naver.com", "")
-    lc.send_letter("김재이", "Facebook_SKKUBamboo", fbcResult)
+    #lc.login("rshtiger@naver.com", "")
+    #lc.send_letter("김재이", "Facebook_SKKUBamboo", fbcResult)
